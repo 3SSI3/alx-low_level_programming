8a0f2e19b38f95dc3114e02028c442d992f6d486
3SSI3 <starsm004@gmail.com>
2022-09-12 15:43:45 +0300

    Name,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print the string using put function
  *
- * Return: Always 0 (Success)
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 
 int main (void)
