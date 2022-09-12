3345a76816b36702d99fec8361267fea4e4ac00d
3SSI3 <starsm004@gmail.com>
2022-09-12 17:04:26 +0300

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
+ * main - prints the string in the put function
  *
  * Description: using the main function
- * this program prints various type sizes
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
