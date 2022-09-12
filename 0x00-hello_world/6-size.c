7b7e10636de2947815ad0171a46c051b50e6da10
3SSI3 <starsm004@gmail.com>
2022-09-12 17:28:58 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * * main - print the string in the put function
- * *
- * * Description: using the main function
- * * this program prints "Programming is like building a multilingual puzzle
- * * Return: 0
- * */
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 int main(void)
 {
 	char c;
