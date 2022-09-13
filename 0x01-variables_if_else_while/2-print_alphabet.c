badf84cbcd6b97b70e08057fc5f26c41f4a3d44b
3SSI3 <starsm004@gmail.com>
2022-09-13 03:51:17 +0300

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 
-	char c;
+	char ch;
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
 	        putchar('\n');
 
 return (0);
