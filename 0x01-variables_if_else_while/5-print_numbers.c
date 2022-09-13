22a477eff20725cc39b889714aa73f75df9f3450
3SSI3 <starsm004@gmail.com>
2022-09-13 04:57:34 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/** 
+ * main - Print digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
