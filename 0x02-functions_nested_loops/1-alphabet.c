55e516198e180615fb374f55b0f773627a24d921
3SSI3 <starsm004@gmail.com>
2022-09-13 17:29:50 +0300

    The alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,17 @@
 #include "main.h"
+
 /**
  * main - print alphabets in lowercas
  *
  * Return: 0
  */
-void  print_alphabet(void);
-
+void  print_alphabet(void)
 {
 	int i;
-
-	i = 'a';
-
-	while (i <= 'z')
+	
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
-		i++;
 	}
 	_putchar('\n');
-	return (0);
 }
