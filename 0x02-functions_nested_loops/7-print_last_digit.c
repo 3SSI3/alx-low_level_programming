eb55132bcf9598218c9e1e46a922a5075e3514ce
3SSI3 <starsm004@gmail.com>
2022-09-14 04:20:18 +0300

    There are only 3 colors, 10 digits, and 7 notes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,12 @@ int print_last_digit(int i)
 	n = i % 10;
 	if (n < 0)
 	{
-		_putchar(-n + 0);
+		_putchar(-n + 48);
 		return (-n);
 	}
 	else
 	{
-		_putchar(n + 0);
+		_putchar(n + 48);
 		return (n);
 	}
 }
