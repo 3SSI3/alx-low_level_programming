4ec9097cd8f1ad0f3f099a8f374fb91a224346d0
3SSI3 <starsm004@gmail.com>
2022-09-14 03:35:51 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,14 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c - The character in ASCII code
+ * @c: The character in ASCII code that will use for the 
+ * argument of the function
  *
  * Return: 1 for the letter 0 for the rest
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c<= 122) || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
