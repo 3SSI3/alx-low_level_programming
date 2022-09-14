18b19ca1a0d71c6a72efcf039bccfabaf95527ea
3SSI3 <starsm004@gmail.com>
2022-09-14 05:06:31 +0300

    Time table 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - Prints time table 9 starting with 0
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int a, b, op;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(0);
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(',');
+			_putchar(' ');
+			if (op <= 9)
+			{
+				_putchar(' ');
+				_putchar(op + 0);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
