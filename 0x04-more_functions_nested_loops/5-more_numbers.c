f9f2777903971657783c1220a212095ed3421d38
3SSI3 <starsm004@gmail.com>
2022-09-16 04:21:53 +0300

    Universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ * from 0 to 14
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (n = 0; n < 15; n++)
+	{
+	if (n >= 10)
+	_putchar((n / 10) + 48);
+	}
+_putchar('\n');
+	}
+}
