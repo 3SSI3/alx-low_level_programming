6933e32d67d2d49ca96e470282eeee4f4509d7b5
3SSI3 <starsm004@gmail.com>
2022-09-21 04:36:11 +0300

    98 Batter st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value of it points to 98.
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
