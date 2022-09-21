1a07651771040d188f7d43cf4e5d285e5525574d
3SSI3 <starsm004@gmail.com>
2022-09-21 05:11:04 +0300

    I dont fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,16 +7,5 @@
  */
 void _puts(char *str)
 {
-	int count = 0;
-
-	while (count >= 0)
-	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
-	}
+	printf("%s\n", str);
 }
