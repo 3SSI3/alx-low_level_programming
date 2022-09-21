e02dbbe9ed5b38ce075edf080696e81c6d16414b
3SSI3 <starsm004@gmail.com>
2022-09-21 05:46:11 +0300

    I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,13 @@
  */
 void print_rev(char *s)
 {
-	int count = 0;
-	
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
+	int i = 0;
 
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
+	while (s[i])
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+
 	_putchar('\n');
 }
