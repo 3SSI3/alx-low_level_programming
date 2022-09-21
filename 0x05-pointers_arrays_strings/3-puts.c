8ac83ae086b561dbae61af6811f2eb966bf0b5a3
3SSI3 <starsm004@gmail.com>
2022-09-21 05:04:38 +0300

    I dont fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line, to stdout.
+ * @str: input string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
