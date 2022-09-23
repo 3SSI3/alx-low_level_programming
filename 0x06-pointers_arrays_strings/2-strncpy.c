23903f9a99e0726f1ee12606b2f6f990e9b36833
3SSI3 <starsm004@gmail.com>
2022-09-23 05:44:19 +0300

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: input value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+	
+	j= 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	for (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	retun (dest);
+}
