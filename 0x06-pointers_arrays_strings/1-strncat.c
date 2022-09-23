6b5c608aedb1f3b956ec3b774695cfc0dfa96208
3SSI3 <starsm004@gmail.com>
2022-09-23 05:26:12 +0300

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,25 +7,24 @@
  * @n: value of bytes from src
  * Return: pointer to the resulting string dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
 
-	int i = 0, j = 0;
-
-	while (*(dest + i) != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-
-	while (j < n)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		*(dest + i) = *(src + j);
-		if (*(src + j) == '\0')
-			break;
+		dest[i] = src[j];
 		i++;
-		j++
+		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
