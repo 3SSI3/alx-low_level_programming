0bc1785b137c1a61dcd0132f4e5260ff8fc2060d
3SSI3 <starsm004@gmail.com>
2022-09-24 05:37:43 +0300

    Toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string,
+ * to uppercase.
+ * @j: string input
+ * Return: pointer to dest
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (j[i] != '\0')
+	{
+		if (j[i] >= 97) && (j[i] <= 122)
+			j[i] = j[i] - 32;
+		i++;
+	}
+
+	return (j);
+}
