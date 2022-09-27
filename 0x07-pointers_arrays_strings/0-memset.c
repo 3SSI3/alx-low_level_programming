bb32a5937f36ece3b9190337cd9dfe37b1f537f4
3SSI3 <starsm004@gmail.com>
2022-09-27 05:02:45 +0300

    Fill memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memset - fills first n bytes with constant byte b.
  * @s: memory area.
@@ -6,12 +7,13 @@
  * @n: bytes filled.
  * Return: the poiner to dest s.
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		s[i] = b;
+		*(s + i) = b;
 
 	return (s);
 }
