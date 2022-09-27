14e41b1a8c1478face1d0f4f332b9b0ed2dd3492
3SSI3 <starsm004@gmail.com>
2022-09-27 16:50:56 +0300

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0')
+	{
 		dest[i] = src[i];
-	for ( ; i < n; i++)
+		i++'
+	}
+	for (i < n; i++)
+	{
 		dest[i] = '\0';
+	}
 
 	return (dest);
 }
