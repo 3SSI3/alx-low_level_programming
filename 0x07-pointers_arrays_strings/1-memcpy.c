49c68293caabd7f9505035ba31bbda7d4f1feccf
3SSI3 <starsm004@gmail.com>
2022-09-27 05:30:45 +0300

    Copy memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		dest[i] = src[i];
+	       *(dest + i) = *(src + i);
 
 	return (dest);
 }
