cb996b4e84ed1f1b084d006ff51ef9b08afaf466
3SSI3 <starsm004@gmail.com>
2022-09-27 05:44:32 +0300

    Locate in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,9 +10,10 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-	
-	return (dest);
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
-
