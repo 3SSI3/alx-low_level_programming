16492c44cc12276667345c3bd871cc66c270b4b4
3SSI3 <starsm004@gmail.com>
2022-09-29 05:34:11 +0300

    Length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: The string.
+ * Return: length of string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1) + 1);
+}
