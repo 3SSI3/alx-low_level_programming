88adb62a062c8044fc4496af33c910dd692d8b59
3SSI3 <112513171+3SSI3@users.noreply.github.com>
2022-10-01 01:37:10 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: argument vector.
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
