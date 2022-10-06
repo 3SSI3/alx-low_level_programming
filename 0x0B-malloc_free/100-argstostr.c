004f94621698b15459e0c78b40eda754aa99bfbe
3SSI3 <112513171+3SSI3@users.noreply.github.com>
2022-10-06 06:04:47 +0300

    Create 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: pointer of an array of char
+ */
+char *argstostr(int ac, char **av)
+{
+	char *aout;
+	int a, b, c, ia;
+
+	if (ac == 0)
+		return (NULL);
+
+	for (a = b = 0; b < ac; b++)
+	{
+		if (av[b] == NULL)
+			return (NULL);
+
+		for ( = 0; av[b][c] != '\0'; c++)
+			a++;
+		a++;
+	}
+
+	aout = malloc((a + 1) * sizeof(char));
+
+	if (aout == NULL)
+	{
+		free(aout);
+		return (NULL);
+	}
+
+	for (b = a = ia = 0; ia < a; c++, ia++)
+	{
+		if (av[b][c] == '\0')
+		{
+			aout[ia] = '\n';
+			b++;
+			ia++;
+			c = 0;
+		}
+		if (ia < a - 1)
+			aout[ia] = av[b][c];
+	}
+	aout[ia] = '\0';
+
+	return (aout);
+}
