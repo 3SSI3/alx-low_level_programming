81dbd57d154ff68d7d81d2cadd1fa895ceb750d2
3SSI3 <starsm004@gmail.com>
2022-10-09 19:23:02 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,7 +4,7 @@
  * *str_concat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * Return: pointer to new space in memory or null
+ * Return:pointer to new space in memory or null
  **/
 char *str_concat(char *s1, char *s2)
 {
