ab165919d2d66fbbfa85d066e63ffa20c0422abb
3SSI3 <starsm004@gmail.com>
2022-10-11 21:10:59 +0300

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+typedef struct dog dog_t;
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
