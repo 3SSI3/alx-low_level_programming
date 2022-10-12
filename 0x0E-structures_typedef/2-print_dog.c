e970c9991a43c6007152498a5ac17f4aade7a84c
3SSI3 <112513171+3SSI3@users.noreply.github.com>
2022-10-12 05:30:01 +0300

    Unconditional Love

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,25 +1,25 @@
 #include "dog.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * print_dog - prints a struct dog.
+ * print_dog - function that prints a struct dog.
  * @d: struct dog.
  * Return: no return.
- */
+ **/
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		if (!(d->name))
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
-		
-		printf("Age: %f\n", d->age);
+if (d)
+{
+if (!(d->name))
+printf("Name: (nil)\n");
+else
+printf("Name: %s\n", d->name);
 
-		if (!(d->owner))
-			printf("Owner: (nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
-	}
+printf("Age: %f\n", d->age);
+
+if (!(d->owner))
+printf("Owner: (nil)\n");
+else
+printf("Owner: %s\n", d->owner);
+}
 }
