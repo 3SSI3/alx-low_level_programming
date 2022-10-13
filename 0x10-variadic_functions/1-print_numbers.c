c94fbebbf02a197118a9e6238f3ecb00def48279
3SSI3 <starsm004@gmail.com>
2022-10-13 21:57:57 +0300

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers
+ * @separator: string to be printed between numbers.
+ * @n: number of integers.
+ *
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int x;
+
+	va_star(ptr, n);
+
+	for (x = 0; x < n; n+++)
+	{
+		printf("%d", va_arg(ptr, int));
+		if (separator && x < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("/n");
+	va_end(ptr);
+}
