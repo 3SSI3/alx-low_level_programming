9a83ef0c5385273b3f589ccf6e6f3428c706d989
3SSI3 <starsm004@gmail.com>
2022-10-21 00:30:10 +0300

    Header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,8 +9,8 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node.
+ *
  * Description: singly linked list node structure
- * for this project.
  */
 typedef struct list_s
 {
@@ -22,7 +22,7 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-ist_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
 
