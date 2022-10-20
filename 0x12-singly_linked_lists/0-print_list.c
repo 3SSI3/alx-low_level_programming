57c3965d65d92447751120dd55addac23cab9f43
3SSI3 <starsm004@gmail.com>
2022-10-20 23:35:57 +0300

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list.
+ * @h: singley linked list to be print.
+ * Return: The number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
