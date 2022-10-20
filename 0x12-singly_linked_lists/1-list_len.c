cfc1e7daa1f45e74174ed8edb24c617795a76ba5
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2022-10-21 00:32:09 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,14 @@
 #include "lists.h"
 /**
- * list_len - returns the number of elements
- * in a linked list_t list.
- * @h: the stringley linked liss to print.
- * Returns: number of elements in a linked list.
+ * list_len - finds the number of nodes in a list
+ * @h: singly linked list
+ * Return: number of elements in a linked list
  */
-
 size_t list_len(const list_t *h)
 {
-	size_t nelem;
+	int i;
 
-	nelem = 0;
-	whiel(h != NULL)
-	{
+	for (i = 0; h; i++)
 		h = h->next;
-		nelem++;
-	}
-	return (nelem);
+	return (i);
 }
