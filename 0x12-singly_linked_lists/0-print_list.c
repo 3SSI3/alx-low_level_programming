348f7a58234fcf58c9bc9a6e16c463f426027103
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2022-10-20 23:55:36 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,16 @@
 size_t print_list(const list_t *h)
 {
 	size_t i;
-
-	for (i = 0; h; i++)
+	
+	i = 0;
+	while (h != NULL)
 	{
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		i++;
 
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
-
 	}
 	return (i);
 }
