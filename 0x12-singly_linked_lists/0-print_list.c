63f0c7a36f2539674ca407739cb1048bc269b8c1
3SSI3 <starsm004@gmail.com>
2022-10-24 17:08:35 +0300

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *tmp;
-	unsigned int i;
+        const list_t *tmp;
+        unsigned int i;
 
-	tmp = h;
-	for (i = 0; tmp; i++)
-	{
-		printf("[%u] %s\n", tmp->len, tmp->str);
-		tmp = tmp->next;
-	}
-	return (i);
+        tmp = h;
+        for (i = 0; tmp; i++)
+        {
+                printf("[%u] %s\n", tmp->len, tmp->str);
+                tmp = tmp->next;
+        }
+        return (i);
 }
