de4fc9ad59feb9daaa8bd037f493d2d79677f84d
3SSI3 <starsm004@gmail.com>
2022-10-25 23:39:25 +0300

    Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the nth of a listint_t linked list.
+ * @head: head of a list.
+ * @index: index of the node.
+ * Return: nth node. If node does not exist, returns NULL.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index && head != NULL; i++)
+	{
+		head = head->next;
+	}
+
+	return (head);
+}
